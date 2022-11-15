da16204bf6d03754cb25b0b0b2dad259a4b72ffb
Pratik Mittal <mittalpratik36@gmail.com>
2022-11-15 13:47:49 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text("Pratik"),
+    );
+  }
+}
+// class MyApp extends StatelessWidget {
+//   Widget built(BuildContext context) {
+//     return MaterialApp(
+//       home: Text('Hello'),
+//     );
+//   }
+// }
