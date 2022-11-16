fe72b288c5cd7f23a514d1f34d9e41b45eb8f9a8
Pratik Mittal <mittalpratik36@gmail.com>
2022-11-16 21:18:02 +0530

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,21 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MyApp());
 }
+// void mian() => runApp(MyApp());
+
+// the above line is only used when there is only onw line in the function
+// and we indicate it by the above arrow.
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Text("Pratik"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Text('this is default!'),
+      ),
     );
   }
 }
-// class MyApp extends StatelessWidget {
-//   Widget built(BuildContext context) {
-//     return MaterialApp(
-//       home: Text('Hello'),
-//     );
-//   }
-// }
